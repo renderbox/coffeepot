f93e25c61e01185735e73d64aff7a07c7c321be4
renderbox <renderbox@gmail.com>
2011-09-04 12:26:52 -0700

    Improvements on arg/kwarg parsing

--- coffeepot/core/helper.py
+++ coffeepot/core/helper.py
@@ -33,24 +33,30 @@ def arg_string_for_js(*args, **kwargs):
         del( kwargs['indent'] )
             
     if args:
-        result = sep.join([ convertValue(x) for x in args ])
-    
+        print "LOADING ARGS"
+        print args
+        result = sep.join([ convertValue(x) for x in args if x != {} ]) # Need to make sure the argument is not a blank dict which can cause a cycle error
+        
+            
     if kwargs:
+        print "LOADING KWARGS"
+        print kwargs
         if args:
             result += sep
         result += "{ %s }" % kwsep.join([ '%s:%s' % ( k, convertValue(v) ) for k, v in kwargs.items() ])
     
     return result
-
-
+    
+    
+# NEED TO ADD CYCLE CHECK MECHANISM BECAUSE OF DICTs
 def convertValue(value):
     if isinstance( value, bool ):                           # Convert Boolean capitalization
         return str(value).lower()
     elif isinstance( value, int ):                          # Values that should not be in quotes
-        return value
+        return str(value)
     elif isinstance( value, dict ):                         # Values that should not be in quotes
-        return arg_string_for_js( [], value )
+        return arg_string_for_js( [], kwargs=value )
     elif isinstance( value, list ) or isinstance( value, tuple ):              # Values that should not be in quotes
-        return '[%s]' % arg_string_for_js( value, {} )
+        return '[%s]' % arg_string_for_js( args=value )
     else:                                                   # Everything else
         return '"%s"' % value
